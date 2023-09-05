9b5cc679626b6f7a72a713d8bcc6acaef91e6c08
tiago-simionato <tiago.simionato5@gmail.com>
2023-09-04 23:05:06 -0300

    Desktop Commitments

--- src/components/Commitments/Commitments.jsx
+++ src/components/Commitments/Commitments.jsx
@@ -17,22 +17,39 @@ export function Commitments() {
       md:text-left
       md:items-start
       md:px-8
+
+      xl:px-[100px]
+      xl:flex-row
+      xl:flex-wrap
+      xl:gap-0
     '>
-      <h2 className="
-        font-medium
-        text-40p/[60px]
-        capitalize
+      <div className='
+        xl:order-2
+        xl:w-[50%]
+      '>
+        <h2 className="
+          font-medium
+          text-40p/[60px]
+          capitalize
 
-        md:min-w-[385px]
-      ">Devår tins bide. </h2>
-      <p className="
-        text-lg/[32px]
-        text-invblack
+          md:min-w-[385px]
 
-        md:max-w-[604px]
-      ">
-        Lande telebelt. Nede kontragisk nisana. Dåvidening sobel. Kana posade. Ädosa. Mynde plande. Ödun ol. Nektig exogt. Rumun durade viplalingar. Nixa dosade. Fasov besat. Diasam mimining povengar. Kromiren teplanat. Självradikalisering. Autofahet dinibel. Virelig saling. Sys fase kroheten.
-      </p>
+          xl:order-2
+          xl:w-[50%]
+          xl:shrink-0
+          xl:mb-3
+        ">Devår tins bide. </h2>
+        <p className="
+          text-lg/[32px]
+          text-invblack
+          
+          md:max-w-[604px]
+          
+          xl:shrink-0
+        ">
+          Lande telebelt. Nede kontragisk nisana. Dåvidening sobel. Kana posade. Ädosa. Mynde plande. Ödun ol. Nektig exogt. Rumun durade viplalingar. Nixa dosade. Fasov besat. Diasam mimining povengar. Kromiren teplanat. Självradikalisering. Autofahet dinibel. Virelig saling. Sys fase kroheten.
+        </p>
+      </div>
 
       <Cards className='
         flex
@@ -45,6 +62,10 @@ export function Commitments() {
         md:gap-x-[184px]
         md:[&>li>div>p]:ml-0
         md:[&>li]:min-w-[180px]
+
+        xl:w-[50%]
+        xl:shrink-0
+        md:gap-x-[138px]
       ' width='w-155p' items={statistics} customTitleStyle='font-semibold text-blue text-64p' />
     </section>
   );
