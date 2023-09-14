2b2e994237b7d3e7389c36e8bdcbfb940c1dd1c6
tiago-simionato <tiago.simionato5@gmail.com>
2023-09-14 15:54:55 -0300

    Help fixes

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -6,24 +6,28 @@ export function Help() {
       items-center
       gap-6
       px-4
-      pt-[90px]
-      pb-[246px]
+      pt-[93.06px]
+      pb-[242.99px]
       text-center
 
       md:px-8
+      md:pt-[72px]
+      md:pb-[199px]
 
       xl:px-[100px]
+      md:pt-32
+      md:pb-[333px]
     ">
-      <label className="font-semibold text-40p">FAQ</label>
+      <label className="font-semibold text-40p/[52px]">FAQ</label>
       <div className="
         font-semibold
         w-full
         bshadow
-        pb-6
+        p-6
         bg-white
       ">
-        <select className="w-full" name="" id="faq">
-          <option className="" value="">Accordion title</option>
+        <select className="w-full select-arrow" name="" id="faq">
+          <option className="font-semibold" value="">Accordion title</option>
           <option value="">Este conteúdo existe apenas para ser substituído pelo conteúdo que você precisar trabalhar. Para isso crie um componente local.</option>
         </select>
       </div>
