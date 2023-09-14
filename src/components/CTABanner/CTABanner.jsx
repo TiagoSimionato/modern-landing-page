e4074895875b73ffe980ceed8a4f61b8474920cd
tiago-simionato <tiago.simionato5@gmail.com>
2023-09-14 13:02:25 -0300

    CTABanner fixes

--- src/components/CTABanner/CTABanner.jsx
+++ src/components/CTABanner/CTABanner.jsx
@@ -1,32 +1,42 @@
 export function CTABanner() {
   return (
     <section className="
+      flex
+      flex-col
+      items-center
       px-4
-      py-24
+      pt-[95.62px]
+      pb-[94.59px]
       text-center
       capitalize
       bg-blue
       text-white
 
       md:text-left
+      md:py-20
       md:px-8
+      md:items-start
 
       xl:px-[100px]
-      xl:flex
-      xl:justify-around
+      xl:py-[76px]
+      xl:flex-row
+      xl:gap-[244px]
+      xl:justify-center
     ">
       <h2 className="
         font-medium
         text-40p
-        mb-6
+        mb-[23.9px]
+        w-375p
 
-        md:max-w-[392px]
-
+        md:w-[392px]
+        md:mb-8
       ">Spetest. Jåspest. Dot. Svenna sitt liv. </h2>
       <p className="
         text-lg/[32px]
+        w-375p
 
-        xl:max-w-[604px]        
+        md:w-[604px]     
       ">
         Dell höbärade. Dism ding. Nefåren fanade. Gigant bel. Peryna varade. Fakoktigt fal. Lussa kvasites. Tektig. Elektroosmos. Kontrada plar. Babybio serad. Nyvida dyslogi. Asa berade. Mint ryggsäcksmodellen. Möbelhund religen. Diapapyren. Bening sapask. Seföl orertad. 
       </p>
